591872243cc9df6accf674e990b495087c60e803
kenken64 <bunnyppl@hotmail.com>
2024-01-17 17:58:34 +0800

    new

--- src/main/java/sg/edu/nus/iss/paf/workshop26/repositories/GameRepository.java
+++ src/main/java/sg/edu/nus/iss/paf/workshop26/repositories/GameRepository.java
@@ -1,15 +1,12 @@
 package sg.edu.nus.iss.paf.workshop26.repositories;
 
 import java.util.List;
-
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.stereotype.Repository;
 
 import sg.edu.nus.iss.paf.workshop26.models.Game;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
@@ -20,12 +17,12 @@ public class GameRepository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
+    
     public List<Game> search(Integer limit,
                                 Integer offset)
     {
-        final PageRequest pageable = PageRequest.of(offset, limit);
-        Query query = new Query().with(pageable);
-
+        Query query = new Query();
+        query.skip(offset).limit(limit);
         return mongoTemplate.find(query, Document.class, "game")
                     .stream()
                     .map(t -> Game.fromJSON(t))
